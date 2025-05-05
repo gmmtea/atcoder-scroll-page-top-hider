a946c16275456fc7b4e5377de9d8a58168256705
gmmtea <>
2025-05-05 22:15:33 +0900

    v1.0.1: add @license metadata

--- hider.user.js
+++ hider.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         AtCoder Scroll Page Top Hider
 // @namespace    https://twitter.com/gmm_tea_cpg
-// @version      1.0.0
+// @version      1.0.1
 // @description  AtCoder のページ内にて「↑ページトップ」リンクを非表示にします
 // @author       gmmtea
 // @match        https://atcoder.jp/*
 // @grant        none
 // @run-at       document-idle
+// @license      MIT
 // ==/UserScript==
 
 (function() {
